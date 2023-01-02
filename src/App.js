f71b924db0d81338910787929bbfa7b6f81bd3b1
ed-aguirre <luis.24_aguirre@outlook.com>
2023-01-02 12:27:03 -0500

    app.js modified

--- src/App.js
+++ src/App.js
@@ -1,25 +1,39 @@
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
+import Wrapper from "./components/wrapper/wrapper";
+import Screen from "./components/screen/screen";
+import ButtonBox from "./components/buttonBox/buttonBox";
+import Button from "./components/button/button";
+
+const btnValues = [
+  ["C", "+-", "%", "/"],
+  [9, 8, 7, "X"],
+  [6, 5, 4, "-"],
+  [3, 2, 1, "+"],
+  [0, ".", "="],
+]
+
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Wrapper>
+      <Screen value="0" />
+        <ButtonBox>
+          {
+            btnValues.flat().map((btn, i) => {
+              return(
+                <Button
+                  key={i}
+                  className={btn === "=" ? "equals" : ""}
+                  value={btn}
+                  onClick={() => {
+                    console.log(`${btn} clicked`);
+                  }}
+                />
+              );
+            })
+          }
+        </ButtonBox>
+    </Wrapper>
   );
-}
+};
 
 export default App;
